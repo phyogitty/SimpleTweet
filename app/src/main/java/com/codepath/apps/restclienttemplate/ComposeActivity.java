c5bf1672f745959fed70456edd9e453211a00bc2
Phyo Khine <pkhine.2020@berkeley.edu>
2019-11-08 23:55:58 -0800

    added all required features for part 2

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,11 +3,15 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -18,13 +22,17 @@ import org.parceler.Parcels;
 
 import okhttp3.Headers;
 
+import static android.graphics.Typeface.BOLD;
+
 public class ComposeActivity extends AppCompatActivity {
 
-    public static final int MAX_TWEET_LENGTH = 140;
+    public static final int MAX_TWEET_LENGTH = 20;
     public static final String TAG = "ComposeActivity";
 
     EditText etCompose;
     Button btnTweet;
+    TextView tvCount;
+    TextView tvWarning;
 
     TwitterClient client;
     @Override
@@ -35,7 +43,34 @@ public class ComposeActivity extends AppCompatActivity {
         client = TwitterApp.getRestClient(this);
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        tvCount = findViewById(R.id.tvCount);
 
+        tvCount.setText("0/" + MAX_TWEET_LENGTH);
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int charCount = etCompose.getText().toString().length();
+                if (charCount <= MAX_TWEET_LENGTH) {
+                    tvCount.setText(charCount + "/" + MAX_TWEET_LENGTH);
+                    tvCount.setTextColor(0xFF33B5E5);
+
+                } else {
+                    tvCount.setText(charCount + "/" + MAX_TWEET_LENGTH);
+                    tvCount.setTextColor(Color.RED);
+                  
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         btnTweet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
